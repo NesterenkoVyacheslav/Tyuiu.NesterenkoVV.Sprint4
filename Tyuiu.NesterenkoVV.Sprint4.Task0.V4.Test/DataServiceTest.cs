f06836e4fd1b69d22838be52ca2b4ec5525b53cb
Вячеслав Нестеренко <Tyuiu.NesterenkoVV@outlook.com>
2025-11-10 12:38:38 +0500

    Add project files.

---
+++ Tyuiu.NesterenkoVV.Sprint4.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.NesterenkoVV.Sprint4.Task0.V4.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint4.Task0.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] nums = new int[] {5, 3, 7, 1, 3, 9, 8, 7, 9 ,4};
+            int wait = 32;
+            var res = ds.GetMultEvenArrEl(nums);
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.NesterenkoVV.Sprint4.Task0.V4/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.NesterenkoVV.Sprint4.Task0.V4.Lib;
+
+namespace Tyuiu.NesterenkoVV.Sprint4.Task0.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Нестеренко В. В. | ИИПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:  Одномерные массивы (статический ввод)                            *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Нестеренко Вячеслав Владимирович | ИИПБ-25-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* статическими значениями в диапазоне от 0 до 9 подсчитать произведение   *");
+            Console.WriteLine("* четных элементов массива.  {5 ,3 ,7 ,1 ,3 ,9 ,8 ,7 ,9 ,4}               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int[] nums = new int[] { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 };
+            var p = ds.GetMultEvenArrEl(nums);
+            Console.WriteLine(p);
+        }
+    }
+}
