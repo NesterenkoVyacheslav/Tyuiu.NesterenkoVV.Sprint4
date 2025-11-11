5ff41f2d8695554094a05234fea80b9ece925951
Вячеслав Нестеренко <Tyuiu.NesterenkoVV@outlook.com>
2025-11-11 14:14:52 +0500

    Task4

---
+++ Tyuiu.NesterenkoVV.Sprint4.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.NesterenkoVV.Sprint4.Task4.V10.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint4.Task4.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds= new DataService();
+            int[,] nums = new int[5, 5] { { 6, 6, 5, 3, 3 }, { 5, 7, 4, 6, 4 }, { 1, 2, 4, 1, 5 }, { 1, 7, 2, 5, 7 }, { 4, 2, 6, 5, 6 } };
+            int[,] wait = new int[5, 5] { { 6, 6, 0, 0, 0 }, { 0, 0, 4, 6, 4 }, { 0, 2, 4, 0, 0 }, { 0, 0, 2, 0, 0 }, { 4, 2, 6, 0, 6 } };
+            var res = ds.Calculate(nums);
+            CollectionAssert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.NesterenkoVV.Sprint4.Task4.V10/Program.cs
@@ -0,0 +1,55 @@
+﻿using Tyuiu.NesterenkoVV.Sprint4.Task4.V10.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint4.Task4.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Нестеренко В. В. | ИИПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:  Двумерные массивы. (ввод с клавиатуры)                           *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Нестеренко Вячеслав Владимирович | ИИПБ-25-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 1 до 7.                          *");
+            Console.WriteLine("* Заменить нечетные элементы массива на 0.                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            int[,] nums = new int[5, 5];
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                for (int j=0; j<nums.GetLength(1); j++)
+                {
+                    Console.Write("Введмте значение " + i +" "+j + "элемента массива: ");
+                    nums[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                for (int j = 0; j < nums.GetLength(1); j++)
+                {
+                    Console.Write(nums[i, j]+" ");
+                }
+                Console.WriteLine();
+            }
+                    Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            var res = ds.Calculate(nums);
+            for (int i = 0; i < res.GetLength(0); i++)
+            {
+                for (int j = 0; j < res.GetLength(1); j++)
+                {
+                    Console.Write(res[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
